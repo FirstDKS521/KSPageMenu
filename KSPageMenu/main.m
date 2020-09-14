b01a488e464208f0f4cbadf63d88eca2e6ba7d33
FirstDKS521 <870815325@qq.com>
2020-09-14 22:35:43 +0800

    Initial Commit

---
+++ KSPageMenu/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KSPageMenu
+//
+//  Created by aDu on 2020/9/14.
+//  Copyright © 2020 aDu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ KSPageMenuTests/KSPageMenuTests.m
@@ -0,0 +1,37 @@
+//
+//  KSPageMenuTests.m
+//  KSPageMenuTests
+//
+//  Created by aDu on 2020/9/14.
+//  Copyright © 2020 aDu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface KSPageMenuTests : XCTestCase
+
+@end
+
+@implementation KSPageMenuTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
